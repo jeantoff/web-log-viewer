8d208ab69465825fb1deb619a56b785d0885526e
cbv <cbv@scan-eco.com>
2013-07-31 12:06:10 +0200

    Added licence and copyright in source files

--- WebLogViewver/Program.cs
+++ WebLogViewver/Program.cs
@@ -1,10 +1,24 @@
 ﻿/*
- * Crée par SharpDevelop.
  * Utilisateur: cbv
  * Date: 02/07/2013
  * Heure: 15:59
  * 
- * Pour changer ce modèle utiliser Outils | Options | Codage | Editer les en-têtes standards.
+ * copyright 2013 Jean-Christophe Bouvard
+ *  Email : nekochat@gmail.com
+* This file is part of WebLogViewer.
+
+    WebLogViewer is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
  */
 using System;
 using System.Windows.Forms;
